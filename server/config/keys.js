34efa7205291e9e8890ad403c8cf87db442279c8
Patrick Emond <patrickdemond@gmail.com>
2019-09-30 13:38:19 -0400

    Adding comment that file should be removed from repo

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,6 @@
+/**
+ * TODO: file is to be removed
+ */
 module.exports = {
     mongoUri: 'mongodb://sakksoftware:sakk11software@ds121282.mlab.com:21282/machealthsci-dev',
     secret: 'akfjlaksdj;lasfjasdfljiasdfasfksldf'
