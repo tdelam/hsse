8574aba33715030fdf5128e38e294ebbd461a866
Patrick Emond <patrickdemond@gmail.com>
2019-09-30 13:33:01 -0400

    Adding documentation to server/routes/administrator

--- server/routes/administrator/HSEAdministratorRoutes.js
+++ server/routes/administrator/HSEAdministratorRoutes.js
@@ -1,5 +1,5 @@
 /**
- * @name SSEAdministratorRoutes.js
+ * @name HSEAdministratorRoutes.js
  * @author Kwadwo Sakyi
  * @description Defines the routing paths for HSE Administrator section of application
  */
